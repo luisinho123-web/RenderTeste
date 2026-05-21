fd294b0f796cb69b38606ecd2b2a99e129586b58
luisinho123-web <lhafigueiredo@minha.fag.edu.br>
2026-05-20 21:30:35 -0300

    Add files via upload
    teste.

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando no Render!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    mensagem: "API online!"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando");
+});
